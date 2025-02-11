3dcd39f1e133cdae4c75cd70eacefd81139ee96d
Psammy <ampadupsamuel1@gmail.com>
2025-02-11 16:36:42 +0000

    Update

--- Main.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Csharp_projects.Tasks;
-
-public class Main
-{
-    static void Main(string[] args)
-    {
-        Book book1 = new Book("BOO1", "Atomic Habits", "James Clear", 4);
-    }
-}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Csharp_projects.Tasks;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Book book = new Book("B001", "Atomic habits", "James Clear", 5);
+        Console.WriteLine($"Book created: {book.Title} by {book.Author}, {book.AvailableCopies} copies available.");
+    }
+}
