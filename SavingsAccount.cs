529c17a621e5bc72a5718cdbbdc713508ef3af9a
Psammy <ampadupsamuel1@gmail.com>
2025-02-18 00:18:32 +0000

    Update

---
+++ SavingsAccount.cs
@@ -0,0 +1,16 @@
+﻿namespace Csharp_projects.Tasks;
+
+public class SavingsAccount : BankAccount
+{
+    private float interestRate;
+
+    //Constructor with superclass initialized attributes
+    public SavingsAccount(float interestRate,
+                            String accountHoldername,
+                            String accountNumber, 
+                            float balance)
+    : base (accountHoldername, accountNumber, balance)
+    {
+        this.interestRate = interestRate;
+    }
+}
